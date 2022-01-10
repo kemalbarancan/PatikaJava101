f4e16f18ca92c726eee86217152bb658aa373d33
Kemal Baran Can <62017122+kemalbarancan@users.noreply.github.com>
2022-01-10 03:13:26 +0300

    Create Test.java

---
+++ MaasHesaplayici/Test.java
@@ -0,0 +1,7 @@
+  public class Test {
+    public static void main(String[] args) {
+        //Fighter f1 =  new Fighter("A", 10, 120,100,50);
+        Employee e1 = new Employee("Kemal", 2000,45,1985);
+        e1.run();
+    }
+}
